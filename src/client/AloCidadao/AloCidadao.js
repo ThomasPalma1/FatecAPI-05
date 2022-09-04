39444af45188f1ad49ac806c62f27d6a2163a0f0
JulianeFreitass <juliane.freitas55@gmail.com>
2022-09-04 12:36:25 -0300

    creating the base react app to run on android

---
+++ src/client/AloCidadao/AloCidadao.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AloCidadao = () => {
+  return (
+    <View>
+      <Text>
+        Alo Cidadão
+      </Text>
+    </View>
+  );
+}
+
+export default AloCidadao;
---
+++ src/client/AloCidadao/index.js
@@ -0,0 +1,5 @@
+import {AppRegistry} from 'react-native';
+import AloCidadao from './AloCidadao';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => AloCidadao);
