9c1eb8f94a68806eafb3ec50959a19730a473f95
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-11-04 20:33:14 -0300

    finishing configuration of files referring to docker
    - changing the position of the .sql file;
    - adding postgres image version to dockerfile.

--- src/server/db/schema/fatecapi-database.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE flaskdb
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'Portuguese_Brazil.1252'
-    LC_CTYPE = 'Portuguese_Brazil.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
