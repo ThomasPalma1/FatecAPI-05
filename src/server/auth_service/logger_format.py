07fc107f800908addb74689614278f64fb42c945
Barbara Bidoia Bidetti <barbarabidetti@gmail.com>
2022-12-05 18:57:56 -0300

    Add filter email

--- src/server/auth_service/logger_format.py
+++ src/server/auth_service/logger_format.py
@@ -1,4 +1,11 @@
 import logging
+from logging import Filter,FileHandler, Formatter
+
+class FiltroSenha(Filter):
+    def filter(self,record):
+        if record.msg.lower().startswith('email'):
+            return False
+        return True
 
 logger_format = logging.basicConfig(
     filename='logRecorder.log',
@@ -6,3 +13,5 @@ logger_format = logging.basicConfig(
     format=f'%(asctime)s | %(levelname)-8s [%(filename)s:%(lineno)d] | %(message)s')
 
 sql_query_executed = logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+FileHandler.addFilter(FiltroSenha())
