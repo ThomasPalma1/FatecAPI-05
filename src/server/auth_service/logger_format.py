e019ea641511c21cc9e9d0d62dc7b4dbfe2bf97d
Barbara Bidoia Bidetti <barbarabidetti@gmail.com>
2022-12-05 20:27:19 -0300

    Alter logger_format

--- src/server/auth_service/logger_format.py
+++ src/server/auth_service/logger_format.py
@@ -1,8 +1,19 @@
 import logging
+from logging import Filter,FileHandler, Formatter
+
+class FiltroSenha(Filter):
+    def filter(self,record):
+        if record.msg.lower().startswith('email'):
+            return False
+        return True
 
 logger_format = logging.basicConfig(
     filename='logRecorder.log',
     level=logging.INFO,
-    format=f'%(asctime)s | %(levelname)s | %(message)s')
+    format=f'%(asctime)s | %(levelname)-8s [%(filename)s:%(lineno)d] | %(message)s')
 
 sql_query_executed = logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+file_handler = FileHandler("logRecorderalt.txt","w")
+
+file_handler.addFilter(FiltroSenha())
