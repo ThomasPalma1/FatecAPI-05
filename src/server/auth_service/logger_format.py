54fa88ae86afc7e09daf1d3a08cf8eb0686b33f9
Barbara Bidoia Bidetti <barbarabidetti@gmail.com>
2022-12-05 18:20:02 -0300

    Add format loggin and filename

--- src/server/auth_service/logger_format.py
+++ src/server/auth_service/logger_format.py
@@ -3,6 +3,6 @@ import logging
 logger_format = logging.basicConfig(
     filename='logRecorder.log',
     level=logging.INFO,
-    format=f'%(asctime)s | %(levelname)s | %(message)s')
+    format=f'%(asctime)s | %(levelname)-8s [%(filename)s:%(lineno)d] | %(message)s')
 
 sql_query_executed = logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
